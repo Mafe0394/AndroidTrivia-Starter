9e122b40fd53c9bca87cf5d61bda72740c4fb213
Mafe0394 <mfvargasm0394@gmail.com>
2021-09-05 12:09:47 -0400

    Controlling events with viewModel

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,17 +1,33 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel:ViewModel() {
     // The current word
-    var word = ""
+    private val _word = MutableLiveData<String>()
+    val word:LiveData<String>
+    get()=_word
+
     // The current score
-    var score = 0
+    private val _score = MutableLiveData<Int>()
+    val score:LiveData<Int>
+    get() =_score
+
+    // hold the game-finished event
+    private val _evetGameFinish=MutableLiveData<Boolean>()
+    val eventGameFinish:LiveData<Boolean>
+    get()=_evetGameFinish
+
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
     init {
+        _word.value = ""
+        _score.value = 0
+        _evetGameFinish.value=false
         resetList()
         nextWord()
     }
@@ -46,22 +62,34 @@ class GameViewModel:ViewModel() {
         wordList.shuffle()
     }
     private fun nextWord() {
-        if (!wordList.isEmpty()) {
+        if (wordList.isEmpty()) {
+            onGameFinish()
+        }else{
             //Select and remove a word from the list
-            word = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
 
     /** Methods for buttons presses **/
 
     fun onSkip() {
-        score--
+        // minus() is null safety
+        _score.value=(score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score++
+        _score.value=(score.value)?.plus(1)
         nextWord()
+    }
+
+    // Method for the game completed event
+    fun onGameFinish(){
+        _evetGameFinish.value=true
+    }
+
+    fun onGameFinishComplete(){
+        _evetGameFinish.value=false
     }
 
     override fun onCleared() {
