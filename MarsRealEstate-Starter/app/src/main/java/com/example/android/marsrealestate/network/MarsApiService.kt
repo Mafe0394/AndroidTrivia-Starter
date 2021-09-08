f4d207301bb76a84ab6301f414e346a66a78b5ab
Mafe0394 <mfvargasm0394@gmail.com>
2021-09-07 20:21:43 -0400

    Using Moshi to convert a Json result into a Model

--- MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -19,24 +19,31 @@
  * This the API that the viewModel will use to communicate with the web Service.*/
 package com.example.android.marsrealestate.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Call
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
 
+// Moshi object to use as a converter factory
+private val moshi=Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 /* Retrofit Object
 Retrofit needs the base URI for the web service and the converter factory.*/
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface MarsApiService {
     @GET("realestate")
     fun getProperties():
-            Call<String>
+            Call<List<MarsProperty>>
 }
 
 // Object created to initialize the Retrofit service
