97fb1323c2f9210a801d0e80c4931cdd34d74071
Mafe0394 <mfvargasm0394@gmail.com>
2021-09-14 18:32:06 -0400

    Repository creation

--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -15,3 +15,42 @@
  */
 
 package com.example.android.devbyteviewer.database
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface VideoDao{
+
+    /* Fetch all the videos from the database.
+    * This method returns LiveData, so that the data displayed in the UI is refreshed
+    * whenever the data in the database is changed*/
+    @Query("select * from databasevideo")
+    fun getVideos(): LiveData<List<DatabaseVideo>>
+
+    /* Insert a list of videos fetched from the network into the database.
+    * Overwrite the database entry if the video is already present in the database,
+    * to do this we use onConflict argument to OnConflictStrategy.REPLACE*/
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(video:List<DatabaseVideo>)
+
+}
+
+@Database(entities = [DatabaseVideo::class],version = 1)
+abstract class VideosDatabase:RoomDatabase(){
+    abstract val videoDao:VideoDao
+}
+
+private lateinit var INSTANCE: VideosDatabase
+
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
+            INSTANCE = Room.databaseBuilder(context.applicationContext,
+                VideosDatabase::class.java,
+                "videos").build()
+        }
+    }
+    return INSTANCE
+}
--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -15,3 +15,44 @@
  */
 
 package com.example.android.devbyteviewer.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.DevByteVideo
+import com.example.android.devbyteviewer.network.DevByteNetwork
+import com.example.android.devbyteviewer.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+/** [database] is a [VideosDatabase] object that works as the class's constructor
+ * parameter to access the Dao methods
+ * */
+class VideosRepository(private val database:VideosDatabase){
+
+    // Transformations.map uses a conversion function to convert one LiveData object into
+    // another LiveData object. it only is calculated when an activity or fragment
+    // is observing the returned LiveData property
+    val videos:LiveData<List<DevByteVideo>> = Transformations.map(database.videoDao.getVideos()){
+        it.asDomainModel()
+    }
+
+    /**
+     * Refresh the videos stored in the offline cache.
+     *
+     * This function uses the IO dispatcher to ensure the database insert database operation
+     * happens on the IO dispatcher. By switching to the IO dispatcher using `withContext` this
+     * function is now safe to call from any thread including the Main thread.
+     *
+     */
+    // Refresh the offline cache.
+    suspend fun refreshVideos(){
+        withContext(Dispatchers.IO){
+            // Fetch from the network
+            val playlist=DevByteNetwork.devbytes.getPlaylist()
+            // Store the playlist in the Room Database
+            database.videoDao.insertAll(playlist.asDatabaseModel())
+        }
+    }
+}
